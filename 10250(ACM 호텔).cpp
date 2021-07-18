cd4846f6226d28fa9302debbdb5511aeee54eb58
wjdwl002 <wjdwl001@khu.ac.kr>
2021-07-18 21:28:32 +0900

    9/18

--- 10250(ACM 호텔).cpp
+++ 10250(ACM 호텔).cpp
@@ -1,4 +1,19 @@
-//
-// Created by 정지원 on 2021/07/18.
-//
+#include <iostream>
+using namespace std;
 
+int main() {
+    int num, H,W,N;
+    cin >> num;
+    for (int i = 0; i<num; i++){
+        cin >> H >> W >> N;
+        int room;
+        if(N%H == 0){
+            room = 100 * H + (N/H);
+        }
+        else{
+            room = 100*(N%H) + (N/H) + 1;
+        }
+        cout << room << endl;
+    }
+    return 0;
+}
--- 2869(달팽이는 올라가고 싶다).cpp
+++ 2869(달팽이는 올라가고 싶다).cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+int main() {
+    int a, b, v;
+    cin >> a >> b >> v;
+
+    int day = (v - b - 1) / (a - b) + 1;
+
+    cout << day;
+
     return 0;
 }
