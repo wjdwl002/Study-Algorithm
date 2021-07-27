f06c23542d025b2214a1f6e341beebf346962f11
wjdwl002 <wjdwl001@khu.ac.kr>
2021-07-27 22:26:07 +0900

    7/27

--- 2447 (별 찍기 - 10).js
+++ 2447 (별 찍기 - 10).js
@@ -1,7 +1,7 @@
 const { resolveNaptr } = require("dns");
 const readline = require("readline");
 const rl = readline.createInterface({input : process.stdin, output:process.stdout});
-
+//2447
 let input;
 let result = "";
 const onInput = (value) => (input = parseInt(value));
